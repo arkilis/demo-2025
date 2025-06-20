8f65ec3d5707db387263d6bc9da78f432d26a3a5
Ben LIU <arkilis@gmail.com>
2025-06-21 06:33:53 +1000

    Initial Commit

---
+++ Canva Demo/Canva_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Canva_DemoApp.swift
+//  Canva Demo
+//
+//  Created by Ben Liu on 21/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct Canva_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
